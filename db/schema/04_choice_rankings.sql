5e5670b6bbc84d78a8c662f784ad2dd91e94953d
Kevinli296 <kevinli296@outlook.com>
2020-11-30 21:22:10 +0000

    Make changes to tables to reflect proper web app flow

--- db/schema/04_choice_rankings.sql
+++ db/schema/04_choice_rankings.sql
@@ -4,6 +4,6 @@ DROP TABLE IF EXISTS choice_rankings CASCADE;
 
 CREATE TABLE choice_rankings (
   id SERIAL PRIMARY KEY NOT NULL,
-  voter_id INTEGER REFERENCES voters(id) ON DELETE CASCADE,
+  choice_id INTEGER REFERENCES choices(id) ON DELETE CASCADE,
   ranking INTEGER NOT NULL
 );
---
+++ db/seeds/04_choice_rankings_seeds.sql
@@ -0,0 +1,12 @@
+-- choice_rankings table seeds here
+
+INSERT INTO choice_rankings (choice_id, ranking)
+VALUES (2, 2),
+(2, 1),
+(1, 3),
+(6, 2),
+(4, 3),
+(6, 1),
+(8, 1),
+(7, 2),
+(7, 3);
