4049aa4eb01788e82de18362b5909889f29318b0
Kevinli296 <kevinli296@outlook.com>
2020-11-30 21:59:15 +0000

    Fix seed data

--- db/seeds/04_choice_rankings_seeds.sql
+++ db/seeds/04_choice_rankings_seeds.sql
@@ -1,12 +1,12 @@
 -- choice_rankings table seeds here
 
 INSERT INTO choice_rankings (choice_id, ranking)
-VALUES (2, 2),
-(2, 1),
-(1, 3),
-(6, 2),
+VALUES (2, 1),
+(1, 2),
+(3, 3),
+(5, 2),
 (4, 3),
 (6, 1),
-(8, 1),
-(7, 2),
-(7, 3);
+(7, 1),
+(9, 2),
+(8, 3);
