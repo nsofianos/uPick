16fd8a24b1ae55811dbe1277d0c9eef260da8270
Connie <leungcnie@gmail.com>
2020-12-01 03:11:43 +0000

    Update GET /polls and POST /polls routes to remove cookie

--- routes/poll.js
+++ routes/poll.js
@@ -12,68 +12,55 @@ module.exports = (db) => { //exporting a FUNCTION that RETURNS a router
 
   // Render poll creation page
   router.get("/create", (req, res) => {
-    // const user = req.session.user_id;
-    const user = true;
-    if (user) {
       res.render('poll_create');
-    } else {
-      res.redirect("/");
-    }
   });
 
   // Add a new poll to database + redirect to voting page
   router.post("/", (req, res) => {
-    req.session.cookie = 'Ak94Vj'; // for testing, REMOVE when done
-    const cookie = req.session.cookie;
     const formData = req.body.text;
     const title = formData[0];
-    const description = formData[1];
-    const choiceNames = formData.slice(2); // array of choice names
-    const pollId = generateRandomString();
+    const email = formData[1];
+    const description = formData[2];
+    const choiceNames = formData.slice(3); // array of choice names
+    const pollKey = generateRandomString();
 
-    db.query(`SELECT creators.id FROM creators WHERE cookie = $1;`, [cookie])
-    .then(data => {
-      // Get specific creator_id from cookie
-      return data.rows[0]; // {id: 1}
-    })
-    .then(creator => {
-      const creator_id = creator.id; // 1
-      const pollParams = [
-        creator_id,
-        title,
-        description,
-        `http://localhost:8080/${pollId}/r`,
-        `http://localhost:8080/${pollId}`
-      ];
+    const pollParams = [
+      email,
+      title,
+      description,
+      `http://localhost:8080/polls/${pollKey}/result`,
+      `http://localhost:8080/polls/${pollKey}`
+    ];
 
-      const queryString = `
-      INSERT INTO polls (creator_id, title, description, admin_link, submission_link)
-      VALUES ($1, $2, $3, $4, $5)
-      RETURNING *;
-      `;
+    const queryString = `
+    INSERT INTO polls (email, title, description, admin_link, submission_link)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+    `;
 
-      // Add poll to polls table and return that poll
-      const poll = db.query(queryString, pollParams).then(data => data.rows[0]);
-      return poll;
-    })
-    .then(poll => {
-      const poll_id = poll.id;
-      const choiceParams = [pollId];
-      for (const name of choiceNames) {
-        choiceParams.push(name);
-      }
+    // Add poll to polls table
+    db.query(queryString, pollParams)
+      .then(data => {
+        return data.rows[0]; // return that poll
+      })
+      .then(poll => {
+        const poll_id = poll.id;
+        const choiceParams = [poll_id];
+        for (const name of choiceNames) {
+          choiceParams.push(name);
+        }
 
-      const queryString = `
+        const queryString = `
       INSERT INTO choices (poll_id, name)
       VALUES ($1, $2);
       `;
 
-      // Add each choice to choices table
-      for (const name of choiceNames) {
-        db.query(queryString, [poll_id, name]);
-      }
-    })
-    res.redirect(`/polls/${pollId}`);
+        // Add each choice to choices table
+        for (const name of choiceNames) {
+          db.query(queryString, [poll_id, name]);
+        }
+      })
+    res.redirect(`/polls/${pollKey}`);
   });
 
   // Render voting + links page
