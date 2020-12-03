ae344b6082cfcf98e8a42582718f719daf972df1
Kevinli296 <kevinli296@outlook.com>
2020-12-03 06:24:14 +0000

    Implement feature to render visualization of all charts

--- routes/index.js
+++ routes/index.js
@@ -19,30 +19,10 @@ module.exports = (db) => {
     ORDER BY polls.id, COUNT(choice_rankings.ranking) DESC;
     `;
 
-    db.query(`SELECT  polls.id AS id,
-    polls.title AS title,
-    choices.name AS choice,
-    COALESCE(SELECT  SUM(cr.ranking)
-             FROM    choice_rankings AS cr
-             WHERE   cr.choice_id = choices.id
-             GROUP BY cr.choice_id), 0) AS summed_votes,
-    COALESCE(SELECT  COUNT(cr2.ranking)
-             FROM    choice_rankings AS cr2
-             WHERE   cr2.choice_id = choices.id
-             GROUP BY cr2.choice_id), 0) AS num_of_votes
-FROM    polls
-    JOIN choices ON polls.id = choices.poll_id
-WHERE   polls.id IN (SELECT id
-                FROM polls
-                ORDER BY random()
-                LIMIT 3)`)
-    .then(data => {
-      console.log(data);
-    });
-
     db.query(queryString)
       .then(data => {
         const index = data.rows;
+        console.log(index);
         // console.log(index);
         let currentPollId = -1; // or whatever you know will never exist
         let currentPollObj = null;
@@ -61,7 +41,8 @@ WHERE   polls.id IN (SELECT id
             currentPollObj.question = row.polls;
             currentPollObj.choicesNRanks = {};
           }
-          currentPollObj.choicesNRanks[row.choices] = row.rank === null ? 1 : row.rank;
+          currentPollObj.choicesNRanks[row.choices] = row.rank === null ? 0 : Number(row.rank);
+          console.log('typeof: ', typeof row.rank);
         }
         // insert the last poll object, since that won't be done in the loop
         // note: what if you have no polls in the database?
