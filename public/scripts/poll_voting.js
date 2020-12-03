acc21b81b17b6914c3a4c732ed75fa5054875bd1
Connie <leungcnie@gmail.com>
2020-12-03 00:24:08 +0000

    Remove share button jQuery and put in new file share.js

--- public/scripts/poll_voting.js
+++ public/scripts/poll_voting.js
@@ -2,33 +2,6 @@ $(() => {
   // Make poll choices draggable
   $('#sortable').sortable();
 
-  // Hide and show links on SHARE button press
-  $('.share-links').hide();
-  $('#share-btn').click(() => {
-    const display = $('.share-links').css('display');
-    if (display === 'none') {
-      $('.share-links').slideDown();
-    } else {
-      $('.share-links').slideUp();
-    }
-  });
-
-  // Copy links to clipboard on COPY button press
-  // Admin link
-  $('#admin-link-btn').click(() => {
-    $('#admin-link').select();
-    document.execCommand('copy');
-    $('#admin-link-btn').after('<p id="copy-alert">Copied!</p>');
-    $('#copy-alert').fadeOut('slow');
-  });
-  // Submission link
-  $('#sub-link-btn').click(() => {
-    $('#sub-link').select();
-    document.execCommand('copy');
-    $('#sub-link-btn').after('<p id="copy-alert2">Copied!</p>');
-    $('#copy-alert2').fadeOut('slow');
-  });
-
   // Submit votes to backend to create choice_rankings
   $('#submit-btn').click(() => {
     let rankedChoices = [];
