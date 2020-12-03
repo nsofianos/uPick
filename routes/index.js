bc66d756f5f084381244e1a592c08334a865f82b
Kevinli296 <kevinli296@outlook.com>
2020-12-03 04:54:48 +0000

    Code cleanup and bug tests

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,35 @@ const router = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const queryString = `
-    SELECT polls.id, polls.title AS polls, choices.name AS choices, choice_rankings.ranking AS rank
+    SELECT polls.id, polls.title AS polls, choices.name AS choices, SUM(choice_rankings.ranking) AS rank
     FROM polls
-    JOIN choices ON poll_id = polls.id
-    JOIN choice_rankings ON choice_id = choices.id
-    WHERE polls.id IN (SELECT id FROM polls
-      ORDER BY RANDOM() LIMIT 3)
+    LEFT JOIN choices ON poll_id = polls.id
+    LEFT JOIN choice_rankings ON choice_id = choices.id
+    GROUP BY polls.id, polls.title, choices.name
+    ORDER BY polls.id, COUNT(choice_rankings.ranking) DESC;
     `;
+
+    db.query(`SELECT  polls.id AS id,
+    polls.title AS title,
+    choices.name AS choice,
+    COALESCE(SELECT  SUM(cr.ranking)
+             FROM    choice_rankings AS cr
+             WHERE   cr.choice_id = choices.id
+             GROUP BY cr.choice_id), 0) AS summed_votes,
+    COALESCE(SELECT  COUNT(cr2.ranking)
+             FROM    choice_rankings AS cr2
+             WHERE   cr2.choice_id = choices.id
+             GROUP BY cr2.choice_id), 0) AS num_of_votes
+FROM    polls
+    JOIN choices ON polls.id = choices.poll_id
+WHERE   polls.id IN (SELECT id
+                FROM polls
+                ORDER BY random()
+                LIMIT 3)`)
+    .then(data => {
+      console.log(data);
+    });
+
     db.query(queryString)
       .then(data => {
         const index = data.rows;
@@ -39,13 +61,13 @@ module.exports = (db) => {
             currentPollObj.question = row.polls;
             currentPollObj.choicesNRanks = {};
           }
-          currentPollObj.choicesNRanks[row.choices] = row.rank;
+          currentPollObj.choicesNRanks[row.choices] = row.rank === null ? 1 : row.rank;
         }
         // insert the last poll object, since that won't be done in the loop
         // note: what if you have no polls in the database?
         polls.push(currentPollObj);
-        console.log(polls);
         const templateVars = { polls };
+        console.log(polls);
         res.render('index', templateVars);
       })
       .catch(err => {
