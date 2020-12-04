7311d3237b119a867b11b359b78b92b35b9ec37f
Kevinli296 <kevinli296@outlook.com>
2020-12-04 05:35:30 +0000

    Add extra visualization of poll data

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,21 @@ const shuffleArray = function(array) {
   }
 }
 
+const calculatePercentage = function(poll) {
+  totalChoices = Object.keys(poll.choicesNRanks).length;
+  sumTotalChoices = (totalChoices * (totalChoices + 1)) / 2;
+  for (const key in poll.choicesNRanks) {
+    let choice = poll.choicesNRanks[key];
+    choice.percentage = Math.round(((choice.rank / (sumTotalChoices * choice.totalVotes)) * 100) * 10) /10;
+  }
+}
+
+// choice.percentage = Math.round(((choice.rankSum / (sumTotalChoices(totalChoices) * totalVotes)) * 100) * 10) / 10;
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const queryString = `
-    SELECT polls.id, polls.title AS polls, choices.name AS choices, SUM(choice_rankings.ranking) AS rank
+    SELECT polls.id, polls.title AS polls, polls.description AS description, choices.name AS choices, SUM(choice_rankings.ranking) AS rank, count(choice_rankings.ranking) as total_rankings
     FROM polls
     LEFT JOIN choices ON poll_id = polls.id
     LEFT JOIN choice_rankings ON choice_id = choices.id
@@ -32,7 +43,6 @@ module.exports = (db) => {
     db.query(queryString)
       .then(data => {
         const index = data.rows;
-        // console.log('index: ', index);
         let currentPollId = -1; // or whatever you know will never exist
         let currentPollObj = null;
         let polls = [];
@@ -41,74 +51,27 @@ module.exports = (db) => {
             // insert the previously constructed poll object into the array
             if (currentPollObj !== null) {
               polls.push(currentPollObj);
+              calculatePercentage(currentPollObj);
             }
-
             // we've found a new unique poll id, create a new object to represent this new poll
             currentPollId = row.id;
 
             currentPollObj = {};
             currentPollObj.question = row.polls;
+            currentPollObj.description = row.description;
+            currentPollObj.rank = row.rank;
             currentPollObj.choicesNRanks = {};
           }
-          currentPollObj.choicesNRanks[row.choices] = row.rank === null ? 0 : Number(row.rank);
+          currentPollObj.choicesNRanks[row.choices] = { rank: (row.rank === null ? 0 : Number(row.rank)), totalVotes: row.total_rankings, percentage: 0 };
         }
         // insert the last poll object, since that won't be done in the loop
         // note: what if you have no polls in the database?
         if (currentPollObj !== null) {
         polls.push(currentPollObj);
+        calculatePercentage(currentPollObj);
         }
         shuffleArray(polls);
         polls = polls.slice(0, 3);
-        const templateVars = { polls };
-        res.render('index', templateVars);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
-  router.get("/randomize", (req, res) => {
-    const queryString = `
-    SELECT polls.id, polls.title AS polls, choices.name AS choices, SUM(choice_rankings.ranking) AS rank
-    FROM polls
-    LEFT JOIN choices ON poll_id = polls.id
-    LEFT JOIN choice_rankings ON choice_id = choices.id
-    GROUP BY polls.id, polls.title, choices.name
-    HAVING COUNT(choice_rankings.id) > 0
-    ORDER BY polls.id, COUNT(choice_rankings.ranking) DESC;
-    `;
-
-    db.query(queryString)
-      .then(data => {
-        const index = data.rows;
-        // console.log(index);
-        let currentPollId = -1; // or whatever you know will never exist
-        let currentPollObj = null;
-        let polls = [];
-        let counter = 1;
-        for (const row of index) {
-          if (currentPollId != row.id) {
-            if (counter > 3) break;
-            counter += 1;
-            // insert the previously constructed poll object into the array
-            if (currentPollObj !== null) {
-              polls.push(currentPollObj);
-            }
-
-            // we've found a new unique poll id, create a new object to represent this new poll
-            currentPollId = row.id;
-
-            currentPollObj = {};
-            currentPollObj.question = row.polls;
-            currentPollObj.choicesNRanks = {};
-          }
-          currentPollObj.choicesNRanks[row.choices] = row.rank === null ? 0 : Number(row.rank);
-        }
-        // insert the last poll object, since that won't be done in the loop
-        // note: what if you have no polls in the database?
-        polls.push(currentPollObj);
         const templateVars = { polls };
         res.render('index', templateVars);
       })
